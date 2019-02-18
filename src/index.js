d8aabf8b432f11caf0b91c8605a63c28030557cf
Ben Strong <whicker86@gmail.com>
2019-02-18 16:42:20 -0700

    finished react-5-afternoon

--- src/index.js
+++ src/index.js
@@ -3,7 +3,14 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
+import {Provider} from 'react-redux'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import store from './store.js'
+
+
+ReactDOM.render(
+<Provider store={store}>
+    <App />
+</Provider>, document.getElementById('root'));
 registerServiceWorker();
 
